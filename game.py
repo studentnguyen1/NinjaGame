c37821d8ed3f632eaa4a01cd9fd4be9492105306
“nguyenvailon” <nguyendeptrai812@gmail.com>
2025-11-30 18:58:17 +0700

    Update Optimization and Cloud

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ import scripts.utils
 from scripts.utils import load_image
 from scripts.utils import load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 
 from scripts.entities import PhysicsEntity
@@ -29,11 +30,11 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-
-
             'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
         }
-        print(self.assets)
+        self.clouds = Clouds(self.assets['clouds'], count=16)
 
 
     
@@ -42,14 +43,24 @@ class Game:
         
         self.tilemap = Tilemap(self, tile_size=16)
 
+        self.scroll = [0, 0]
+
 
     def run(self):
         while True:
-            self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0, 0))
 
-            self.tilemap.render(self.display)            
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)            
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
+            
             
             print(self.tilemap.physics_rects_around(self.player.pos))
 
