46efc85cd2a4b1c0cdfbb37a074e2cc414e30ae8
“nguyenvailon” <nguyendeptrai812@gmail.com>
2025-12-13 01:38:26 +0700

    Update level transition

--- game.py
+++ game.py
@@ -93,8 +93,18 @@ class Game:
 
             self.screenshake = max(0, self.screenshake - 1)
 
+            if not len(self.enemies):
+                self.transition += 1
+                if self.transition > 30:
+                    self.level += 1
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+
             if self.dead:
                 self.dead += 1
+                if self.dead >= 10:
+                    self.transition = min(30, self.transition + 1)
                 if self.dead > 40:
                     self.load_level(self.level)
             
@@ -183,7 +193,15 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
+
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(transition_surf, (255,255,255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8)
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0, 0))
+
             screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
+
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)
             pygame.display.update()
